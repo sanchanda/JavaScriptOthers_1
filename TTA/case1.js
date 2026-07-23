8c4db50583de8adfc96161e9bdd8f8a440a1cdef
sanchanda <santoshkumar.chanda1@gmail.com>
2026-07-23 14:40:14 +0530

    Add case1.js with normalizeJsBasicsLabel function

---
+++ TTA/case1.js
@@ -0,0 +1,23 @@
+//Input: normalizeJsBasicsLabel(" Login Button ")
+//Expected: "js-basic-login-button"
+
+function normalizeJsBasicsLabel(label) {
+    let cleanLabel1 = label.replace(/[@@]/g, ' ');
+    let cleanLabel = cleanLabel1.replace(/[^a-zA-Z0-9\s]/g, '');
+    let lowerCaseValue = cleanLabel.toLowerCase().trim().split(/\s+/).join("-");
+    return lowerCaseValue ? "js-basic-" + lowerCaseValue : "js-basic";
+
+}
+
+
+// let b = normalizeJsBasicsLabel("---Search@@Box---");
+// console.log(b);
+
+// b = normalizeJsBasicsLabel("Login Button");
+// console.log(b);
+
+// b = normalizeJsBasicsLabel(" ");
+// console.log(b);
+
+// b = normalizeJsBasicsLabel("User Profile: Edit!");
+// console.log(b);
